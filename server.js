4b015cbd4fb4ab5dc0c59a4bafe178457c8eab2e
DmitriyPavlichenko <pavlichenko.dmitriy@gmail.com>
2021-12-19 22:20:48 +0200

    Added deployment support

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static(__dirname + '/dist/<FOLDER_NAME>'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-);
+app.get('/*', (req,res,next) => {
+  res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 8000);
