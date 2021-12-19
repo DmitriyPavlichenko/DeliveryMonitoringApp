1f0af5c37cf1535647337382a0d9a9c1bdcfe6cc
DmitriyPavlichenko <pavlichenko.dmitriy@gmail.com>
2021-12-19 22:38:06 +0200

    Added deployment support

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<FOLDER_NAME>'));
+app.use(express.static('<FOLDER_NAME>'));
 
 app.get('/*', (req,res,next) => {
   res.sendFile('/');
