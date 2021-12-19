dd7bd608abc9b5bcc0641cd042df80504129c40f
ArtemVasylchuck <artem.vasylchuck@gmail.com>
2021-12-19 12:14:51 +0200

    added login html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,15 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  public login: string = "";
-  public password: string = "";
+export class LoginComponent implements OnInit {
 
   constructor() { }
 
-
-  setCredentials() {
-
+  ngOnInit(): void {
   }
+
 }
