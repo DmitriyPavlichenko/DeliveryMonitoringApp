c743d18fdab877143de5d27ef3b51c8fee5ef6a2
DmitriyPavlichenko <pavlichenko.dmitriy@gmail.com>
2021-12-21 22:46:55 +0200

    Added authentication and primitive realization of employee component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+﻿import {Component, OnInit} from '@angular/core';
+
+import {User} from '@app/authorization/_models';
+import {AuthenticationService} from "@app/authorization/_services";
+
+@Component({templateUrl: 'home.component.html'})
+export class HomeComponent implements OnInit {
+  loading = false;
+  user: User;
+
+  constructor(private authenticationService: AuthenticationService) {
+    this.user = authenticationService.userValue;
+  }
+
+  ngOnInit() {
+    this.loading = true;
+    /*
+    some stuff
+    */
+  }
+}
